2d1f9514b5db155d1a0942a1099732654cd946b1
Rahul Vyas <rahulvyas@plaak.com>
2019-03-19 00:42:50 +0530

    tradingview charts added

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import TradingViewWidget, { Themes } from 'react-tradingview-widget';
 import '../App.css';
 class Charts extends Component {
   render() {
     return (
       <div className="Charts">
-      
+       <TradingViewWidget
+    symbol="NASDAQ:AAPL"
+    theme={Themes.DARK}
+    locale="en"
+    autosize
+  />
       </div>
     );
   }
