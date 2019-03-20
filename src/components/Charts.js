994f35a8c5b874d14e5f1f0d5948364d2d3b75f3
Rahul Vyas <rahulvyas@plaak.com>
2019-03-20 13:30:39 +0530

    Charts & Navbar update

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -3,8 +3,16 @@ import TradingViewWidget, { Themes } from 'react-tradingview-widget';
 import '../App.css';
 class Charts extends Component {
   render() {
+    
     return (
       <div className="Charts">
+      <div className="ChartsHeader">
+      <div className="BlockTitle">Charts</div>
+      <div class="tab">
+  <button className="tablinks">Candle</button>
+  <button className="tablinks">Line</button>
+  <button className="tablinks">Depth</button>
+</div></div>
        <TradingViewWidget
     symbol="NASDAQ:AAPL"
     theme={Themes.DARK}
